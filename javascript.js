62d596ec68f78f71807cbe9aacbba33b0f8b9690
BLisme <tranbaolam006@gmail.com>
2025-04-12 17:22:26 +0800

    Finish the game UI

--- javascript.js
+++ javascript.js
@@ -3,70 +3,87 @@ function getComputerChoice(){
     temp*=30;
     if(temp <10) return "rock"; //Rock
     if(temp <20) return "paper"; //Paper
-    if(temp <30) return "scissor"; //Scissor
+    if(temp <30) return "scissors"; //Scissor
 }
 
-function getHumanChoice(){
-    let inputH=prompt("What is your move? Valid move is rock, paper, scissor");
-    inputH=inputH.toLowerCase();
-    return inputH;
-}
+let divc= document.querySelector(".choice");
+let divtext=document.querySelector(".resultText");
+let display=document.querySelector(".resultDisplay");
+let humanChoice,computerChoice;
 
-function playGame(){
-    let humanScore=0;
-    let computerScore=0;
-    function playRound(humanChoice1,computerChoice1){
-        function parseC(choice){
-            if(choice =='rock') return 1;
-            else if(choice =='paper') return 2;
-            else if(choice=='scissor') return 3;
-            else return 4;
+divtext.style.fontSize ="50px";
+display.style.fontSize ="50px";
+divc.addEventListener("click", function(e){
+    let target= e.target.closest("button");
+    
+    switch (target.className){
+        case "rock":{
+            
+            humanChoice="rock";
+            computerChoice=getComputerChoice();
+            playRound(humanChoice,computerChoice);
+            break;
         }
-        let parseHum=parseC(humanChoice1);
-        let parseCom=parseC(computerChoice1);
-        if(parseHum == 4){
-            console.log("You input is wrong");
+        case "paper":{
+            humanChoice="paper";
+            computerChoice=getComputerChoice();
+            playRound(humanChoice,computerChoice);
+            break;
+        }
+        case "scissors":{
+            humanChoice="scissors";
+            computerChoice=getComputerChoice();
+            playRound(humanChoice,computerChoice);
+            break;
+        }
+    }
+    display.textContent=`Currently, your score is ${humanScore}, meanwhile the computer score is ${computerScore}`;
+})
+let humanScore=0;
+let computerScore=0;
+
+function playRound(humanChoice1,computerChoice1){
+    function parseC(choice){
+        if(choice =='rock') return 1;
+        else if(choice =='paper') return 2;
+        else if(choice=='scissors') return 3;
+        else return 4;
+    }
+    let parseHum=parseC(humanChoice1);
+    let parseCom=parseC(computerChoice1);
+    if(parseHum == 4){
+        divtext.textContent="You input is wrong";
+        return;
+    }
+    if(parseHum == parseCom){
+        divtext.textContent=`Draw! You are lucky! You both choose ${humanChoice1}`;
+        return;
+    }
+    else{
+        if(parseHum - parseCom == 1){
+            divtext.textContent="You just win!"+` ${humanChoice1} beats ${computerChoice1}`;
+            //only 3,2 and 2,1
+            humanScore++;
             return;
         }
-        if(parseHum ==parseCom){
-            console.log("Draw! You are lucky!");
+        else if(parseHum - parseCom == -1){
+            divtext.textContent="You just lose!"+ `${computerChoice1} beats ${humanChoice1}`;
+            //only 1,2 and 2,3
+            computerScore++;
+            return;
+        }
+        else if(parseHum>parseCom){
+            divtext.textContent="You just lose!"+ `${computerChoice1} beats ${humanChoice1}`;
+            //only 3 1
+            computerScore++;
+            return;
         }
         else{
-            if(parseHum - parseCom == 1){
-                console.log("You are the winner!"+` ${humanChoice1} beats ${computerChoice1}`);
-                //only 3,2 and 2,1
-                humanScore++;
-            }
-            else if(parseHum - parseCom == -1){
-                console.log("You are the loser!"+ `${computerChoice1} beats ${humanChoice1}`);
-                //only 1,2 and 2,3
-                computerScore++;
-            }
-            else if(parseHum>parseCom){
-                console.log("You are the loser!"+ `${computerChoice1} beats ${humanChoice1}`);
-                //only 3 1
-                computerScore++;
-            }
-            else{
-                console.log("You are the winner"+ `${humanChoice1} beats ${computerChoice1}`);
-                //only 1 3
-                humanScore++;
-            }
+            divtext.textContent="You just win!"+ `${humanChoice1} beats ${computerChoice1}`;
+            //only 1 3
+            humanScore++;
+            return;
         }
     }
-    for(let i =1;i<=5;i++){
-        let humanChoice=getHumanChoice();
-        let computerChoice=getComputerChoice();
-        playRound(humanChoice,computerChoice);
-    }
-    if(humanScore>computerScore){
-        console.log("Overall you are the winner!");
-    }
-    if(humanScore<computerScore){
-        console.log("So sad! You are loser");
-    }
-    if(humanScore==computerScore){
-        console.log("Overall draw!");
-    }
+    
 }
-playGame();
